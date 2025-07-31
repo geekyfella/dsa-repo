ff4eaad68796d6353f00b67fe0e73ca94f928401
Ojasvi Kumar <ojasvikumar10@gmail.com>
2025-07-31 22:42:48 +0530

    pattern repo created

---
+++ patterns/pattern_1.cpp
@@ -0,0 +1,21 @@
+//
+// Created by ojasv on 31-07-2025.
+//
+#include "pattern_1.h"
+# include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+for ( int i = 0; i<4 ; i++){
+    for ( int j = 0; j<4; j++){
+         cout << "*";
+    }
+cout << endl;
+}
+return 0;
+}
+// Output
+// ****
+// ****
+// ****
+// ****
---
+++ patterns/pattern_1.h
@@ -0,0 +1,16 @@
+//
+// Created by ojasv on 31-07-2025.
+//
+
+#ifndef PATTERN_1_H
+#define PATTERN_1_H
+
+
+
+class pattern_1 {
+
+};
+
+
+
+#endif //PATTERN_1_H
