1626f0b775984a113541e78dcf8422ef491dd892
Ojasvi Kumar <ojasvikumar10@gmail.com>
2025-08-01 09:31:23 +0530

    pattern_4 added

---
+++ patterns/pattern_4.cpp
@@ -0,0 +1,24 @@
+//
+// Created by ojasv on 01-08-2025.
+//
+
+#include "pattern_4.h"
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    for (int i = 1; i < 6; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << i;
+        }
+        cout << endl;
+    }
+    return 0;
+}
+//Output
+
+// 1
+// 22
+// 333
+// 4444
+// 55555
---
+++ patterns/pattern_4.h
@@ -0,0 +1,16 @@
+//
+// Created by ojasv on 01-08-2025.
+//
+
+#ifndef PATTERN_4_H
+#define PATTERN_4_H
+
+
+
+class pattern_4 {
+
+};
+
+
+
+#endif //PATTERN_4_H
