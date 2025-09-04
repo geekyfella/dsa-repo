6f4433a5e07fc2820adab2e584cdffd0be6a0ba1
Ojasvi Kumar <ojasvikumar10@gmail.com>
2025-09-04 23:08:48 +0530

    update digit concept

--- patterns/digitConcept.cpp
+++ patterns/digitConcept.cpp
@@ -1,6 +1,3 @@
-//
-// Created by ojasv on 04-09-2025.
-//
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -8,9 +5,9 @@ int DigitExtraction(int number) {
     cout << "NUmber entered by the user: " << number << endl;
     while (number > 0) {
         int LastDigit = number % 10;
-        cout << "Last Digit of the number is: "<< LastDigit << endl;
+        cout << "Last Digit: "<< LastDigit << endl;
         number = number / 10;
-        cout << " Second Last Digit of the number is: "<< number << endl;
+        cout << " Second Last Digit: "<< number << endl;
     }
     return 0;
 }
