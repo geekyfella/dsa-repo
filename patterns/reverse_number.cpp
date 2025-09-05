c0faae22ac9cb3503c9bc4e7a2bafcaa0df4ba6d
Ojasvi Kumar <ojasvikumar10@gmail.com>
2025-09-05 13:18:00 +0530

    add reverse_number

---
+++ patterns/reverse_number.cpp
@@ -0,0 +1,19 @@
+//
+// Created by ojasv on 05-09-2025.
+//
+#include <bits/stdc++.h>
+using namespace std;
+
+int ReverseNumber(int number) {
+    int rev_num = 0;
+    while (number > 0) {
+        int last_digit = number % 10;
+        number = number / 10;
+        rev_num = (rev_num * 10) + last_digit;
+    }
+    return rev_num;
+}
+int main() {
+    cout <<"reverse of number: " << ReverseNumber(314) << endl;
+    return 0;
+}
